688c3487326707ffcb2f2423b5547edc1f776cb0
Ridoy <ridoy@workspaceit.com>
2024-08-23 21:41:13 +0600

    udpated db connection function

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,10 +1,7 @@
 const mongoose = require("mongoose");
 exports.connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected : ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
